38fd9071dd402b33bb71d8c046d69664578dab56
Eva Demkova <evaademkova@gmail.com>
2020-10-24 16:59:36 +0200

    owner details styling

--- resources/views/owners/show.blade.php
+++ resources/views/owners/show.blade.php
@@ -2,31 +2,32 @@
 
 @section('content')
 
-<h1>{{$owner->surname}} {{$owner->first_name}}</h1>
-<p>Address: {{$owner->address}}</p>
-<p>Telephone: {{$owner->phone_number}}</p>
-<p>E-mail: {{$owner->email}}</p>
+<div class="owner-container">
+    <div class="owner">
+        <div class="owner__info">
+            <h1>{{$owner->surname}} {{$owner->first_name}}</h1>
+            <p>Address: {{$owner->address}}</p>
+            <p>Telephone: {{$owner->phone_number}}</p>
+            <p>E-mail: {{$owner->email}}</p>
+        </div>
+        <div class="owner__btns">
+            <a href={{action('OwnerController@edit', $owner->id) }}><button class="btn btn-warning">Update owner</button></a>
+            <a href={{action('OwnerController@index') }}><button class="btn btn-secondary">Go back to owners list</button></a>
+        </div> 
+    </div>
 
-<p>
-    <a href={{action('AnimalController@create', $owner->id) }}>Create a new Animal</a>
-</p>
-
-<p>
-    <a href={{action('OwnerController@edit', $owner->id) }}>Update owner</a>
-</p>
-
-<p>
-    <a href={{action('OwnerController@index') }}>Go back to owners list</a>
-</p>
-
-<div class="animals">
-    <h2>Animals</h2>
-    <ul>
-
-    @foreach ($owner->animals as $animal)
-        <li>{{$animal->name}} <img src={{asset($animal->photo)}}></li>
-    @endforeach
-    </ul>
+    <div class="animals">
+        <h2>Animals</h2>
+        <div class="animals__details"> 
+            <a href={{action('AnimalController@create', $owner->id) }}><button class="new-animal-btn">Create a new Animal</button></a>
+            @foreach ($owner->animals as $animal)
+                <div class="animal-item">
+                    <img src={{asset($animal->photo)}}>
+                    <p class="name">{{$animal->name}} </p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 
 @endsection
