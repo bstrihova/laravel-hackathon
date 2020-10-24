0877aa7fdb2eb498e25ef9f2ed820a74606d8fb5
Eva Demkova <evaademkova@gmail.com>
2020-10-24 18:56:04 +0200

    user form update

--- resources/views/owners/edit.blade.php
+++ resources/views/owners/edit.blade.php
@@ -2,27 +2,29 @@
 
 @section('content')
 
-<form action="{{ action('OwnerController@store') }}" method="post">
-    
-@csrf
-    <label for="first_name">First Name:
-        <input type="text" name="first_name" value='{{$owner->first_name}}'>
-    </label><br>
-    <label for="surname">Surname:</label>
-    <input type="text" name="surname" value='{{$owner->surname}}'><br>
+    <div class="new-user-form">
+        <h3>New Owner</h3>
+        <form action="{{ action('OwnerController@update', $owner->id) }}" method="post">
+            @csrf
+            <label for="first_name">First Name:</label>
+                <input type="text" name="first_name" value='{{$owner->first_name}}'><br>
+            
+            <label for="surname">Surname:</label>
+            <input type="text" name="surname" value='{{$owner->surname}}'><br>
 
-    <label for="address">Adress: </label>
-    <input type="text" name="address" value='{{$owner->address}}'><br>
+            <label for="address">Adress: </label>
+            <input type="text" name="address" value='{{$owner->address}}'><br>
 
-    <label for="email">Email: </label>
-    <input type="text" name="email" value='{{$owner->email}}'><br>
+            <label for="email">Email: </label>
+            <input type="text" name="email" value='{{$owner->email}}'><br>
 
-    <label for="phone_number">Phone Number:</label>
-    <input type="text" name="phone_number" value='{{$owner->phone_number}}'><br>
+            <label for="phone_number">Phone Number:</label>
+            <input type="text" name="phone_number" value='{{$owner->phone_number}}'><br>
 
-    <button type="submit">Edit owner</button>
-</form>
+            <button type="submit" class="btn btn-primary">Edit owner</button>
+        </form>
 
-<a href={{action('OwnerController@show', $owner->id)}}>Go back to Animal</a>
+        <a href="{{action('OwnerController@show', $owner->id)}}"><button class="btn btn-secondary">Go back to Owner</button></a>
+    </div>
 
 @endsection
